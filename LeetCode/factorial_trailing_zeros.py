6768cc570eba5ca9f67645513ad7cf2ba9579cb9
Brandon Gao <reterclose@gmail.com>
2015-07-05 11:40:11 +0800

    Add Factorial Trailing Zeroes.

--- LeetCode/factorial_trailing_zeros.py
+++ LeetCode/factorial_trailing_zeros.py
@@ -3,3 +3,15 @@ Given an integer n, return the number of trailing zeroes in n!.
 
 Note: Your solution should be in logarithmic time complexity.
 """
+
+
+class Solution:
+    # @param {integer} n
+    # @return {integer}
+    def trailingZeroes(self, n):
+        x = 5
+        ans = 0
+        while n >= x:
+            ans += n / x
+            x *= 5
+        return ans
